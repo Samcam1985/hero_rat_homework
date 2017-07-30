163aa385690d7ef87e4a91f1f6eabe6a65423f2b
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-30 17:04:14 +0100

    food has replenishment value test passed

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -13,4 +13,7 @@ it("Should have a name", function(){
   assert.strictEqual(food.name, "Pizza");
 });
 
+it("should have replenishment value", function(){
+  assert.strictEqual(food.replenishment_value, 50);
+})
 });
