2579c83fe2ad8b81dc8c6214459b82968933d942
Samantha Campbell <samantha.l.campbell1985@gmail.com>
2017-07-30 17:58:37 +0100

    Hero has collection of tasks and add task test passed

--- hero.js
+++ hero.js
@@ -2,9 +2,14 @@ var Hero = function(name, health, favourite_food) {
   this.name = name;
   this.health = health;
   this.favourite_food = favourite_food;
+  this.tasks = [];
 
   Hero.prototype.sayName = function() {
     return "Hello, My name is " + this.name;
+  }
+
+  Hero.prototype.addTask = function(task) {
+    this.tasks.push(task);
   }
 }
 
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,11 +1,15 @@
 var assert = require('assert');
 var Hero = require('../hero');
+var Task = require('../task');
 
 describe("Hero", function(){
   var hero;
+  var task;
 
   beforeEach(function(){
     hero = new Hero("Oswald", 100, "Pizza")
+    task1 = new Task(10, "High", 50)
+    task2 = new Task(10, "Low", 20)
     
   });
 
@@ -23,6 +27,11 @@ describe("Hero", function(){
 
   it("Should talk saying their name", function(){
     assert.strictEqual(hero.sayName(), "Hello, My name is Oswald");
+  });
+
+  it("Should add a task", function(){
+    hero.addTask(task1);
+    assert.strictEqual(hero.tasks.length, 1);
   });
 })
 
